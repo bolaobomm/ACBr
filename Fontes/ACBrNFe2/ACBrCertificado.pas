30c98d7398bd0f40ce140e3a2ca65c39559aaeec
anfm <anfm@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2013-03-20 12:26:10 +0000

    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk@4994 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrNFe2/ACBrCertificado.pas
+++ Fontes/ACBrNFe2/ACBrCertificado.pas
@@ -1,9 +1,53 @@
+{******************************************************************************}
+{ Projeto: Componente ACBrNFe                                                  }
+{  Biblioteca multiplataforma de componentes Delphi para emissão de Nota Fiscal}
+{ eletrônica - NFe - http://www.nfe.fazenda.gov.br                          }
+{                                                                              }
+{ Direitos Autorais Reservados (c) 2008 Wemerson Souto                         }
+{                                       Daniel Simoes de Almeida               }
+{                                       André Ferreira de Moraes               }
+{                                                                              }
+{ Colaboradores nesse arquivo:                                                 }
+{                                                                              }
+{  Você pode obter a última versão desse arquivo na pagina do Projeto ACBr     }
+{ Componentes localizado em http://www.sourceforge.net/projects/acbr           }
+{                                                                              }
+{                                                                              }
+{  Esta biblioteca é software livre; você pode redistribuí-la e/ou modificá-la }
+{ sob os termos da Licença Pública Geral Menor do GNU conforme publicada pela  }
+{ Free Software Foundation; tanto a versão 2.1 da Licença, ou (a seu critério) }
+{ qualquer versão posterior.                                                   }
+{                                                                              }
+{  Esta biblioteca é distribuída na expectativa de que seja útil, porém, SEM   }
+{ NENHUMA GARANTIA; nem mesmo a garantia implícita de COMERCIABILIDADE OU      }
+{ ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral Menor}
+{ do GNU para mais detalhes. (Arquivo LICENÇA.TXT ou LICENSE.TXT)              }
+{                                                                              }
+{  Você deve ter recebido uma cópia da Licença Pública Geral Menor do GNU junto}
+{ com esta biblioteca; se não, escreva para a Free Software Foundation, Inc.,  }
+{ no endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.          }
+{ Você também pode obter uma copia da licença em:                              }
+{ http://www.opensource.org/licenses/lgpl-license.php                          }
+{                                                                              }
+{ Daniel Simões de Almeida  -  daniel@djsystem.com.br  -  www.djsystem.com.br  }
+{              Praça Anita Costa, 34 - Tatuí - SP - 18270-410                  }
+{                                                                              }
+{******************************************************************************}
+
+{******************************************************************************
+|* Historico
+|*
+|* 19/03/2013: André Ferreira de Moraes
+|*  - Versão inicial da classe ACBrCertificado
+|* 20/03/2013: Rafael Batiati
+|*  - Inclusão do comando SelecionarCertificado
+******************************************************************************}
 unit ACBrCertificado;
 
 interface
 
 uses
-  Classes, Windows, SysUtils, Variants, WinCrypt, ACBrMSXML2_TLB, ACBrUtil;
+  Classes, Windows, SysUtils, Variants, WinCrypt, ACBrUtil;
 
 type
 
@@ -30,11 +74,17 @@ type
     property NumeroSerial: string read FNumeroSerial;
   end;
 
+
+
   TACBrStore = class(TComponent)
   private
     FStore: HCERTSTORE;
     FStoreName: string;
     FCertificados: TList;
+
+    xCryptUIDlgSelectCertificateFromStore:Function(hCertStore: HCERTSTORE; hwnd: HWND; pwszTitle, pwszDisplayString: LPCWSTR;
+  dwDontUseColumn, dwFlags: DWORD; pvReserved: Pointer):PCCERT_CONTEXT;StdCall;
+
     function GetCount: Integer;
     function GetItem(Index: Integer): TACBrCertificado;
     procedure CarregaCertificados(hStore: HCERTSTORE);
@@ -45,6 +95,7 @@ type
     procedure AbrirCertificados(const AStoreName: string);
     property Items[Index: Integer]: TACBrCertificado read GetItem; default;
     property Count: Integer read GetCount;
+    function SelecionarCertificado: string;
   published
     property StoreName: string read FStoreName write FStoreName;
   end;
@@ -133,6 +184,12 @@ begin
   inherited Create(AOwner);
   FCertificados := TList.Create();
   FStoreName := '';
+
+  if not FunctionDetect( 'cryptui.dll', 'CryptUIDlgSelectCertificateFromStore', @xCryptUIDlgSelectCertificateFromStore) then
+   begin
+     xCryptUIDlgSelectCertificateFromStore := NIL ;
+     raise Exception.Create( ACBrStr( 'Erro ao carregar a função CryptUIDlgSelectCertificateFromStore de: cryptui.dll' ) ) ;
+   end ;
 end;
 
 destructor TACBrStore.Destroy;
@@ -172,6 +229,33 @@ begin
   FStore := CertOpenSystemStore(0, PChar(StoreName));
   if FStore <> nil then
     CarregaCertificados(FStore);
+end;
+
+function TACBrStore.SelecionarCertificado: string;
+var
+  hCtx: PCCERT_CONTEXT;
+  Titulo, Pergunta : PWideChar;
+begin
+  if FStore = nil then
+    raise Exception.Create( ACBrStr( 'Execute o comando AbrirCertificados' ) ) ;
+    
+  try
+    begin
+      Titulo   := PWideChar(WideString('Certificado(s) Digital(is) disponível(is)'));
+      Pergunta := PWideChar(WideString('Selecione o Certificado Digital para uso no aplicativo'));
+      hCtx := xCryptUIDlgSelectCertificateFromStore(FStore, 0,
+        Titulo, Pergunta, $000000010, 0, nil);
+
+      if (Assigned(hCtx)) then
+      begin
+        Result := RemoveString(' ',ByteArrayToStr(hCtx^.pCertInfo^.serialNumber.pbData,hCtx^.pCertInfo^.serialNumber.cbData));
+      end;
+    end;
+  finally
+    begin
+      CertFreeCertificateContext(hCtx);
+    end;
+  end;
 end;
 
 end.
