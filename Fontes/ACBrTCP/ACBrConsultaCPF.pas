f4618e8c53506c6c5dd5997f5617d79eed385848
regyssilveira <regyssilveira@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2013-09-09 12:22:43 +0000

    - ACBrConsultaCPF: Contribuição do usuário jeter.rabelo (http://www.projetoacbr.com.br/forum/index.php?/topic/11761-consulta-de-cpf-receita-fazenda/#entry73473)
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk@5841 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

---
+++ Fontes/ACBrTCP/ACBrConsultaCPF.pas
@@ -0,0 +1,260 @@
+{******************************************************************************}
+{ Projeto: Componentes ACBr                                                    }
+{  Biblioteca multiplataforma de componentes Delphi para interação com equipa- }
+{ mentos de Automação Comercial utilizados no Brasil                           }
+{                                                                              }
+{ Direitos Autorais Reservados (c) 2004 Daniel Simoes de Almeida               }
+{                                                                              }
+{ Colaboradores nesse arquivo:                                                 }
+{                                                                              }
+{  Você pode obter a última versão desse arquivo na pagina do  Projeto ACBr    }
+{ Componentes localizado em      http://www.sourceforge.net/projects/acbr      }
+{                                                                              }
+{ Esse arquivo usa a classe  SynaSer   Copyright (c)2001-2003, Lukas Gebauer   }
+{  Project : Ararat Synapse     (Found at URL: http://www.ararat.cz/synapse/)  }
+{                                                                              }
+{  Esta biblioteca é software livre; você pode redistribuí-la e/ou modificá-la }
+{ sob os termos da Licença Pública Geral Menor do GNU conforme publicada pela  }
+{ Free Software Foundation; tanto a versão 2.1 da Licença, ou (a seu critério) }
+{ qualquer versão posterior.                                                   }
+{                                                                              }
+{  Esta biblioteca é distribuída na expectativa de que seja útil, porém, SEM   }
+{ NENHUMA GARANTIA; nem mesmo a garantia implícita de COMERCIABILIDADE OU      }
+{ ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral Menor}
+{ do GNU para mais detalhes. (Arquivo LICENÇA.TXT ou LICENSE.TXT)              }
+{                                                                              }
+{  Você deve ter recebido uma cópia da Licença Pública Geral Menor do GNU junto}
+{ com esta biblioteca; se não, escreva para a Free Software Foundation, Inc.,  }
+{ no endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.          }
+{ Você também pode obter uma copia da licença em:                              }
+{ http://www.opensource.org/licenses/lgpl-license.php                          }
+{                                                                              }
+{ Daniel Simões de Almeida  -  daniel@djsystem.com.br  -  www.djsystem.com.br  }
+{              Praça Anita Costa, 34 - Tatuí - SP - 18270-410                  }
+{                                                                              }
+{******************************************************************************}
+{******************************************************************************
+|* Historico
+|*
+|* 07/08/2013: Primeira Versao - Adaptador conforme ACBRConsultaCNPJ
+|*    Daniel - schrsistemas@gmail.com
+******************************************************************************}
+
+{$I ACBr.inc}
+
+unit ACBrConsultaCPF;
+
+interface
+
+uses
+  SysUtils, Classes, ACBrSocket;
+
+type
+  EACBrConsultaCPFException = class ( Exception );
+
+  { TACBrConsultaCPF }
+
+  TACBrConsultaCPF = class(TACBrHTTP)
+  private
+    FViewState: String;
+    FNome: String;
+    FSituacao: String;
+    FCPF: String;
+    FDigitoVerificador: String;
+    FEmissao: String;
+    FCodCtrlControle: String;
+    Function GetCaptchaURL: String;
+
+    function VerificarErros(Str: String): String;
+    function LerCampo(Texto: TStringList; NomeCampo: AnsiString): String;
+  public
+    procedure Captcha(Stream: TStream);
+    function Consulta(const ACPF, ACaptcha: String;
+      ARemoverEspacosDuplos: Boolean = False): Boolean;
+  published
+    property CPF: String Read FCPF Write FCPF;
+    property Nome: String Read FNome;
+    property Situacao: String Read FSituacao;
+    property DigitoVerificador: String Read FDigitoVerificador;
+    property Emissao: String Read FEmissao;
+    property CodCtrlControle: String Read FCodCtrlControle;
+  end;
+
+implementation
+
+uses
+  ACBrUtil, synacode, synautil, strutils;
+
+function StrEntreStr(Str, StrInicial, StrFinal: String; ComecarDe: Integer = 1): String;
+var
+  Ini, Fim: Integer;
+begin
+  Ini:= PosEx(StrInicial, Str, ComecarDe) + Length(StrInicial);
+  if Ini > 0 then
+  begin
+    Fim:= PosEx(StrFinal, Str, Ini);
+    if Fim > 0 then
+      Result:= Copy(Str, Ini, Fim - Ini)
+    else
+      Result:= '';
+  end
+  else
+    Result:= '';
+end;
+
+function TACBrConsultaCPF.GetCaptchaURL : String ;
+var
+  URL, Html: String;
+begin
+  try
+    Self.HTTPGet('http://www.receita.fazenda.gov.br/aplicacoes/atcta/cpf/consultapublica.asp');
+    Html := Self.RespHTTP.Text;
+
+    URL := 'http://www.receita.fazenda.gov.br' +
+           StrEntreStr(Html, 'alt='+
+                        QuotedStr(ACBrStr('Imagem com os caracteres anti robô')) + ' src='+'''', '''');
+
+    FViewState := StrEntreStr(Html, '<input type=hidden id=viewstate name=viewstate value='+'''', '''');
+
+    Result := StringReplace(URL, 'amp;', '', []);
+  except
+    on E: Exception do
+    begin
+      raise EACBrConsultaCPFException.Create('Erro na hora de obter a URL do captcha.'+#13#10+E.Message);
+    end;
+  end;
+end;
+
+procedure TACBrConsultaCPF.Captcha(Stream: TStream);
+begin
+  try
+    HTTPGet(GetCaptchaURL);
+    if HttpSend.ResultCode = 200 then
+    begin
+      HTTPSend.Document.Position := 0;
+      Stream.CopyFrom(HttpSend.Document, HttpSend.Document.Size);
+      Stream.Position := 0;
+    end;
+  Except on E: Exception do begin
+    raise EACBrConsultaCPFException.Create('Erro na hora de fazer o download da imagem do captcha.'+#13#10+E.Message);
+  end;
+  end;
+end;
+
+function TACBrConsultaCPF.VerificarErros(Str: String): String;
+  var
+    Res: String;
+begin
+  Res:= '';
+  if Res = '' then
+    if Pos( ACBrStr('Erro na Consulta'), Str) > 0 then
+      Res:= 'Catpcha errado.';
+
+  if Res = '' then
+    if Pos(ACBrStr('O número do CPF não é válido. Verifique se o mesmo foi digitado c'+
+    'orretamente.'), Str) > 0 then
+      Res:= 'O número do CPF não é válido. Verifique se o mesmo foi digitado'+
+      ' corretamente.';
+
+  if Res = '' then
+    if Pos(ACBrStr('Não existe no Cadastro de Pessoas Jurídicas o número de CPF infor'+
+    'mado. Verifique se o mesmo foi digitado corretamente.'), Str) > 0 then
+      Res:= 'Não existe no Cadastro de Pessoas Jurídicas o número de CPF info'+
+      'rmado. Verifique se o mesmo foi digitado corretamente.';
+
+  if Res = '' then
+    if Pos(ACBrStr('a. No momento não podemos atender a sua solicitação. Por favor tent'+
+    'e mais tarde.'), Str) > 0 then
+      Res:= 'Erro no site da receita federal. Tente mais tarde.';
+
+  Result:= ACBrStr(Res);
+end;
+
+function TACBrConsultaCPF.LerCampo(Texto : TStringList ; NomeCampo : AnsiString
+  ) : String ;
+var
+  i : integer;
+  linha : String;
+begin
+  NomeCampo := ACBrStr(UpperCase(NomeCampo));
+  Result := '';
+  for i := 0 to Texto.Count-1 do
+  begin
+    linha := ACBrStr(UpperCase(Texto[i]));
+    if Pos(NomeCampo, linha) > 0 then
+    begin
+      Result := Trim(StringReplace(linha, NomeCampo, ' ',[rfReplaceAll]));
+      break;
+    end;
+  end
+end;
+
+function TACBrConsultaCPF.Consulta(const ACPF, ACaptcha: String;
+  ARemoverEspacosDuplos: Boolean): Boolean;
+var
+  Post: TStringStream;
+  Erro, Html: String;
+  Resposta : TStringList;
+  I: Integer;
+begin
+  Post:= TStringStream.Create('');
+  try
+    Post.WriteString('origem=comprovante&');
+    Post.WriteString('viewstate=' + EncodeURLElement(fviewstate)+'&');
+    Post.WriteString('txtCPF='+OnlyNumber(ACPF)+'&');
+    Post.WriteString('captcha='+Trim(ACaptcha)+'&');
+    Post.WriteString('captchaAudio=&');
+    Post.WriteString('Enviar=Consultar&');
+    Post.WriteString('search_type=CPF');
+    Post.Position:= 0;
+
+    HttpSend.Clear;
+    HttpSend.Document.Position:= 0;
+    HttpSend.Document.CopyFrom(Post, Post.Size);
+    HTTPSend.MimeType := 'application/x-www-form-urlencoded';
+    HTTPPost('http://www.receita.fazenda.gov.br/aplicacoes/atcta/cpf/ConsultaPublicaExibir.asp');
+
+    Erro := VerificarErros(RespHTTP.Text);
+
+    if Erro = '' then
+    begin
+      Result:= True;
+      Resposta := TStringList.Create;
+      try
+        Resposta.Text := StripHTML(RespHTTP.Text);
+        RemoveEmptyLines( Resposta );
+
+        //Resposta.Text := AnsiToUtf8(Resposta.Text);
+        //DEBUG: Resposta.SaveToFile('C:\cpf.txt');
+
+        FCPF      := LerCampo(Resposta,'No do CPF:');
+        FNome     := LerCampo(Resposta,'Nome da Pessoa Física:');
+        FSituacao := LerCampo(Resposta,'Situação Cadastral:');
+        FEmissao  := LerCampo(Resposta,'Comprovante emitido às:');
+        FCodCtrlControle   := LerCampo(Resposta,'Código de controle do comprovante:');
+        FDigitoVerificador := LerCampo(Resposta,'Digito Verificador:');
+
+      finally
+        Resposta.Free;
+      end ;
+
+      if Trim(FNome) = '' then
+        raise EACBrConsultaCPFException.Create('Não foi possível obter os dados.');
+
+      if ARemoverEspacosDuplos then
+      begin
+        FNome := RemoverEspacosDuplos(FNome);
+      end;
+    end
+    else
+    begin
+      Result:= False;
+      raise EACBrConsultaCPFException.Create(Erro);
+    end;
+  finally
+    Post.Free;
+  end;
+end;
+
+end.
+
--- Fontes/ACBrTCP/ACBrTCPReg.pas
+++ Fontes/ACBrTCP/ACBrTCPReg.pas
@@ -42,7 +42,7 @@ procedure Register;
 implementation
 
 Uses ACBrSocket, ACBrCEP, ACBrIBGE, ACBrNFPws, ACBrCNIEE, ACBrSuframa, ACBrDownload,
-   SysUtils, ACBrIBPTax, ACBrConsultaCNPJ, ACBrCotacao;
+   SysUtils, ACBrIBPTax, ACBrConsultaCNPJ, ACBrConsultaCPF, ACBrCotacao;
 
 {$IFNDEF FPC}
    {$R ACBrTCP.dcr}
@@ -52,7 +52,7 @@ procedure Register;
 begin
   RegisterComponents('ACBr',
     [ TACBrTCPServer, TACBrCEP, TACBrIBGE, TACBrNFPws, TACBrCNIEE, TACBrSuframa,
-      TACBrDownload, TACBrIBPTax, TACBrConsultaCNPJ, TACBrCotacao ]
+      TACBrDownload, TACBrIBPTax, TACBrConsultaCNPJ, TACBrConsultaCPF, TACBrCotacao ]
   );
 end;
 
