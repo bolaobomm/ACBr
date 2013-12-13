a3f5e4665236c197a42ab0c9db20cf3c99c08f55
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2013-12-13 18:29:28 +0000

    Melhorias na geração da TAG IE para o Destinatário.
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk@6211 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/PCN2/pcnNFeW.pas
+++ Fontes/PCN2/pcnNFeW.pas
@@ -585,20 +585,27 @@ begin
 
   Gerador.IDNivel := 'E01';
 
+  // Alterado por Italo em 13/12/2013
   if nfe.infNFe.Versao >= 3.10 then
-    Gerador.wCampo(tcStr, 'E16a', 'indIEDest', 01, 01, 1, indIEDestToStr(nfe.Dest.indIEDest), DSC_INDIEDEST);
+    Gerador.wCampo(tcStr, 'E16a', 'indIEDest', 01, 01, 1, indIEDestToStr(nfe.Dest.indIEDest), DSC_INDIEDEST)
+  else nfe.Dest.indIEDest := inContribuinte;
 
-  // Inscrição Estadual
-  if nfe.Dest.IE = 'ISENTO' then
-    Gerador.wCampo(tcStr, 'E17', 'IE ', 00, 14, 1, nfe.Dest.IE, DSC_IE)
-  else if (trim(nfe.Dest.IE) <> '') or (nfe.Ide.modelo <> 65)  then
-    Gerador.wCampo(tcStr, 'E17', 'IE     ', 00, 14, 1, SomenteNumeros(nfe.Dest.IE), DSC_IE);
+  // Alterado por Italo em 13/12/2013
+  if nfe.Dest.indIEDest <> inNaoContribuinte then
+   begin
+     // Inscrição Estadual
+     if nfe.Dest.IE = 'ISENTO' then
+       Gerador.wCampo(tcStr, 'E17', 'IE ', 00, 14, 1, nfe.Dest.IE, DSC_IE)
+     else if (trim(nfe.Dest.IE) <> '') or (nfe.Ide.modelo <> 65)  then
+     Gerador.wCampo(tcStr, 'E17', 'IE     ', 00, 14, 1, SomenteNumeros(nfe.Dest.IE), DSC_IE);
 
-//  if (length(nfe.Dest.CNPJCPF) = 11) and (SomenteNumeros(nfe.Dest.IE) <> '') then
-//    Gerador.wAlerta('E17', 'IE', DSC_IE, ERR_MSG_INVALIDO); // Para MG produtor rural possui CPF e IE
-  if (FOpcoes.ValidarInscricoes) and (nfe.Dest.IE <> '') and (nfe.Dest.IE <> 'ISENTO') then
-    if not ValidarIE(nfe.Dest.IE, UF) then
-      Gerador.wAlerta('E17', 'IE', DSC_IE, ERR_MSG_INVALIDO);
+   //  if (length(nfe.Dest.CNPJCPF) = 11) and (SomenteNumeros(nfe.Dest.IE) <> '') then
+   //    Gerador.wAlerta('E17', 'IE', DSC_IE, ERR_MSG_INVALIDO); // Para MG produtor rural possui CPF e IE
+     if (FOpcoes.ValidarInscricoes) and (nfe.Dest.IE <> '') and (nfe.Dest.IE <> 'ISENTO') then
+       if not ValidarIE(nfe.Dest.IE, UF) then
+         Gerador.wAlerta('E17', 'IE', DSC_IE, ERR_MSG_INVALIDO);
+   end;
+
   Gerador.wCampo(tcStr, 'E18', 'ISUF   ', 08, 09, 0, nfe.Dest.ISUF, DSC_ISUF);
   if (FOpcoes.ValidarInscricoes) and (nfe.Dest.ISUF <> '') then
     if not ValidarISUF(nfe.Dest.ISUF) then
