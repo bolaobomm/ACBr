da8496547ad87a294363e604218a44d58f0cd0d7
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2014-02-07 12:50:05 +0000

    Para o provedor Ginfes o valor da aliquota é dividada por 100 ao gerar o XML.
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk@6366 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrNFSe/pnfsNFSeW.pas
+++ Fontes/ACBrNFSe/pnfsNFSeW.pas
@@ -327,7 +327,6 @@ begin
     case FProvedor of
      proGovBR,
      proPronim,
-     {proGINFES,}
      proISSNet:   Gerador.wCampoNFSe(tcDe4, '#25', 'Aliquota', 01, 05, 1, NFSe.Servico.Valores.Aliquota, '');
 
      proRecife:   if NFSe.OptanteSimplesNacional = snSim
@@ -335,6 +334,8 @@ begin
                     else Gerador.wCampoNFSe(tcDe2, '#25', 'Aliquota', 01, 05, 1, NFSe.Servico.Valores.Aliquota, '');
 
      proSimplISS: Gerador.wCampoNFSe(tcDe2, '#25', 'Aliquota', 01, 05, 0, NFSe.Servico.Valores.Aliquota, '');
+
+     proGINFES:   Gerador.wCampoNFSe(tcDe4, '#25', 'Aliquota', 01, 05, 1, (NFSe.Servico.Valores.Aliquota / 100), '');
 
      else         Gerador.wCampoNFSe(tcDe4, '#25', 'Aliquota', 01, 05, 0, NFSe.Servico.Valores.Aliquota, '');
     end;
