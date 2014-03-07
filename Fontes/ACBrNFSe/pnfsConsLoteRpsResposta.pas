0dd0313ec389c0effb4d74d8ffd1a8cb07108c5a
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2014-03-07 16:31:39 +0000

    Correção, esta duplicando o fechamento da TAG Nfse.
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk@6437 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrNFSe/pnfsConsLoteRpsResposta.pas
+++ Fontes/ACBrNFSe/pnfsConsLoteRpsResposta.pas
@@ -270,8 +270,8 @@ begin
           if Leitor.rExtrai(iNivel + 3, 'Nfse') <> ''
            then begin
             ListaNfse.FCompNfse[i].FNfse.XML := {'<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>'+}
-                                                Leitor.Grupo +
-                                                '</Nfse>';
+                                                Leitor.Grupo {+
+                                                '</Nfse>'};
             // Grupo da TAG <InfNfse> *****************************************************
             if Leitor.rExtrai(iNivel + 4, 'InfNfse') <> ''
              then begin
