aeb2ef0d282af476cf807a53b9e15cb2846f841e
embarbosa <embarbosa@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2014-06-16 17:15:56 +0000

    [-] ACBrTEFD DEMO: Units faltantes adicionadas ao arquivo de projeto do Delphi;
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk@7018 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Exemplos/ACBrTEFD/TEFDemo.dpr
+++ Exemplos/ACBrTEFD/TEFDemo.dpr
@@ -7,7 +7,9 @@ uses
   Unit3 in 'Unit3.pas' {Form3},
   Unit4 in 'Unit4.pas' {Form4},
   Unit5 in 'Unit5.pas' {Form5},
-  ConfiguraSerial in 'ConfiguraSerial.pas' {frConfiguraSerial};
+  ConfiguraSerial in 'ConfiguraSerial.pas' {frConfiguraSerial},
+  Unit6 in 'Unit6.pas' {Form6},
+  Unit7 in 'Unit7.pas' {Form7};
 
 {$R *.res}
 
