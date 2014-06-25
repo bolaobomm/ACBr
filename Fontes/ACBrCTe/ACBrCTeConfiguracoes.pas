c2ed23be81bd6becbc183f66bdc10a4319717761
embarbosa <embarbosa@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2014-06-25 20:31:21 +0000

    - Correção de memory leak. Veja: http://www.projetoacbr.com.br/forum/index.php?/topic/16257-memory-leak-getcertificado/
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk@7070 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrCTe/ACBrCTeConfiguracoes.pas
+++ Fontes/ACBrCTe/ACBrCTeConfiguracoes.pas
@@ -500,16 +500,20 @@ begin
          if (Pos('2.16.76.1.3.3', Propriedades) > 0) then
           begin
             Lista      := TStringList.Create;
-            Lista.Text := Propriedades;
-            for K := 0 to Lista.Count-1 do
-             begin
-               if (Pos('2.16.76.1.3.3', Lista.Strings[K]) > 0) then
-                begin
-                  FCNPJ := StringReplace(Lista.Strings[K], '2.16.76.1.3.3=', '', [rfIgnoreCase]);
-                  FCNPJ := OnlyNumber(HexToAscii(RemoveString(' ', FCNPJ)));
-                  break;
-                end;
-             end;
+			try
+              Lista.Text := Propriedades;
+              for K := 0 to Lista.Count-1 do
+               begin
+                 if (Pos('2.16.76.1.3.3', Lista.Strings[K]) > 0) then
+                  begin
+                    FCNPJ := StringReplace(Lista.Strings[K], '2.16.76.1.3.3=', '', [rfIgnoreCase]);
+                    FCNPJ := OnlyNumber(HexToAscii(RemoveString(' ', FCNPJ)));
+                    break;
+                  end;
+               end;
+			finally
+			  Lista.Free;
+			end
             break;
           end;
          Extension := nil;
