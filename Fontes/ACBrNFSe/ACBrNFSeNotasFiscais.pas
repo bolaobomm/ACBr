37e601cbaaa7bef8c567e5f619718d9f660061a6
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2014-09-02 12:01:03 +0000

    Correção para obter de forma correta o CNPJ ou CPF do Prestador ao ler o XML do disco. Proposto por Lucas Peres.
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk@7363 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrNFSe/ACBrNFSeNotasFiscais.pas
+++ Fontes/ACBrNFSe/ACBrNFSeNotasFiscais.pas
@@ -899,17 +899,17 @@ begin
                     Pos('<Prestador><CpfCnpj><Cnpj>', ArquivoXML.Text) + 26,
                     Pos('</Cnpj></CpfCnpj>',ArquivoXML.Text) - (Pos('<Prestador><CpfCnpj><Cnpj>', ArquivoXML.Text) + 26));
 
-       if Trim(CNPJ) = '' then
+       if ((Length(CNPJ) <> 11) and (Length(CNPJ) <> 14)) or (Trim(CNPJ) = '') then
           CNPJ:= Copy(ArquivoXML.Text,
                       Pos('<Prestador><CpfCnpj><Cpf>', ArquivoXML.Text) + 25,
                       Pos('</Cpf></CpfCnpj>',ArquivoXML.Text) - (Pos('<Prestador><CpfCnpj><Cpf>', ArquivoXML.Text) + 25));
 
-       if Trim(CNPJ) = '' then
+       if ((Length(CNPJ) <> 11) and (Length(CNPJ) <> 14)) or (Trim(CNPJ) = '') then
           CNPJ := Copy(ArquivoXML.Text,
                       Pos('<Prestador><Cnpj>', ArquivoXML.Text) + 17,
                       Pos('</Cnpj>',ArquivoXML.Text) - (Pos('<Prestador><Cnpj>', ArquivoXML.Text) + 17));
 
-       if Trim(CNPJ) = '' then
+       if ((Length(CNPJ) <> 11) and (Length(CNPJ) <> 14)) or (Trim(CNPJ) = '') then
           CNPJ:= Copy(ArquivoXML.Text,
                       Pos('<Prestador><Cpf>', ArquivoXML.Text) + 16,
                       Pos('</Cpf>',ArquivoXML.Text) - (Pos('<Prestador><Cpf>', ArquivoXML.Text) + 16));
