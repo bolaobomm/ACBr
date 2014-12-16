cf34a23ee1799efbcf3346059e023c4ef96f8dc5
ispinheiro <ispinheiro@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2014-12-16 17:32:10 +0000

    Correções de refectory
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk@7955 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrMDFe/ACBrMDFeWebServices.pas
+++ Fontes/ACBrMDFe/ACBrMDFeWebServices.pas
@@ -514,7 +514,7 @@ end;
 procedure TWebServicesBase.AssinarXML(AXML: String; MsgErro: String);
 begin
   {$IFDEF ACBrMDFeOpenSSL}
-   if not(NotaUtil.Assinar(AXML,
+   if not(MDFeUtil.Assinar(AXML,
                            FConfiguracoes.Certificados.Certificado,
                            FConfiguracoes.Certificados.Senha,
                            FDadosMsg, FMsg)) then
