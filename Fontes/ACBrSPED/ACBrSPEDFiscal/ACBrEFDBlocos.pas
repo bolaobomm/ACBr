5b349a38cb84bb343c46916356eeecaf2cf8033d
ispinheiro <ispinheiro@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2014-12-18 14:00:17 +0000

    Atualização de versão do leiaute do arquivo.
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk@7967 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrSPED/ACBrSPEDFiscal/ACBrEFDBlocos.pas
+++ Fontes/ACBrSPED/ACBrSPEDFiscal/ACBrEFDBlocos.pas
@@ -161,10 +161,12 @@ type
                              vlVersao101,  // Código 002 - Versão 101 Ato COTEPE 01/01/2009
                              vlVersao102,  // Código 003 - Versão 102 Ato COTEPE 01/01/2010
                              vlVersao103,  // Código 004 - Versão 103 Ato COTEPE 01/01/2011
-                             vlVersao104,  // Código 005 - Versão 104 Ato COTEPE 01/01/2012
+                             vlVersao104,  // Código 005 - Versão 104 Ato COTEPE 01/07/2012
                              vlVersao105,  // Código 006 - Versão 105 Ato COTEPE 01/07/2012
                              vlVersao106,  // Código 007
-                             vlVersao107   // Código 008 - Válido a partir de 01/01/2014
+                             vlVersao107   // Código 008
+                             vlVersao108   // Código 009
+                             vlVersao109   // Código 010
                              );
   TACBrVersaoLeiaute = TACBrCodVer;
 
