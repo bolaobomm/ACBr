a82c02ceb89547dd7cb8997d5dfa2f527c30a042
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2015-03-04 18:49:23 +0000

    04/03/2015 [*] Inclusão das URLs do QR-Code para DF.     Por: Gilson_ST
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk@8458 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrNFe2/ACBrNFeUtil.pas
+++ Fontes/ACBrNFe2/ACBrNFeUtil.pas
@@ -2033,7 +2033,7 @@ begin
    13: Result := DFeUtil.SeSenao(AAmbiente = taProducao, 'http://sistemas.sefaz.am.gov.br/nfceweb/formConsulta.do', 'http://homnfce.sefaz.am.gov.br/nfceweb/formConsulta.do'); // AM
    29: Result := DFeUtil.SeSenao(AAmbiente = taProducao, 'http://nfe.sefaz.ba.gov.br/servicos/nfce/default.aspx',   'http://hnfe.sefaz.ba.gov.br/servicos/nfce/default.aspx'); // BA
    23: Result := DFeUtil.SeSenao(AAmbiente = taProducao, '', ''); //CE
-   53: Result := DFeUtil.SeSenao(AAmbiente = taProducao, '', ''); //DF
+   53: Result := DFeUtil.SeSenao(AAmbiente = taProducao, 'http://dec.fazenda.df.gov.br/nfce',               'http://dec.fazenda.df.gov.br/nfce'); //DF
    32: Result := DFeUtil.SeSenao(AAmbiente = taProducao, '', ''); //ES
    52: Result := DFeUtil.SeSenao(AAmbiente = taProducao, '', ''); //GO
    21: Result := DFeUtil.SeSenao(AAmbiente = taProducao, 'http://www.nfce.sefaz.ma.gov.br/portal/consultaNFe.do', 'http://www.hom.nfce.sefaz.ma.gov.br/portal/consultaNFe.do'); // MA
@@ -2077,7 +2077,7 @@ begin
    13: urlUF := DFeUtil.SeSenao(AAmbiente = taProducao, 'http://sistemas.sefaz.am.gov.br/nfceweb/consultarNFCe.jsp', 'http://homnfce.sefaz.am.gov.br/nfceweb/consultarNFCe.jsp'); // AM
    29: urlUF := DFeUtil.SeSenao(AAmbiente = taProducao, 'http://nfe.sefaz.ba.gov.br/servicos/nfce/modulos/geral/NFCEC_consulta_chave_acesso.aspx', 'http://hnfe.sefaz.ba.gov.br/servicos/nfce/modulos/geral/NFCEC_consulta_chave_acesso.aspx'); // BA
    23: urlUF := DFeUtil.SeSenao(AAmbiente = taProducao, '', ''); // CE
-   53: urlUF := DFeUtil.SeSenao(AAmbiente = taProducao, '', ''); // DF
+   53: urlUF := DFeUtil.SeSenao(AAmbiente = taProducao, 'http://dec.fazenda.df.gov.br/ConsultarNFCe.aspx', 'http://dec.fazenda.df.gov.br/ConsultarNFCe.aspx'); // DF
    32: urlUF := DFeUtil.SeSenao(AAmbiente = taProducao, '', ''); // ES
    52: urlUF := DFeUtil.SeSenao(AAmbiente = taProducao, '', ''); // GO
    21: urlUF := DFeUtil.SeSenao(AAmbiente = taProducao, 'http://www.nfce.sefaz.ma.gov.br/portal/consultarNFCe.jsp', 'http://www.hom.nfce.sefaz.ma.gov.br/portal/consultarNFCe.jsp'); // MA
