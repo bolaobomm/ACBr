7a014cb69ab55b8bde51379c1a834d5a8905dde4
juliomar <juliomar@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2015-05-05 20:20:21 +0000

    Modificação para vir desabilitado por default a pesquisa do IBGE http://www.projetoacbr.com.br/forum/index.php?/topic/21250-consulta-c%C3%B3digo-ibge-acbrconsultacnpj/#entry137916
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk@8907 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrTCP/ACBrConsultaCNPJ.pas
+++ Fontes/ACBrTCP/ACBrConsultaCNPJ.pas
@@ -369,7 +369,7 @@ constructor TACBrConsultaCNPJ.Create(AOwner: TComponent);
 begin
   inherited;
   FCNAE2 := TStringList.Create;
-  FPesquisarIBGE := True;
+  FPesquisarIBGE := False;
   fACBrIBGE := TACBrIBGE.Create(nil);
 end;
 
