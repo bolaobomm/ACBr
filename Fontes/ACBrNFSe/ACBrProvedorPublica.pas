98d6ca2ef29faf00496de4b0bf1ca4a1ca1f50e4
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2015-05-14 18:32:47 +0000

    Correção e melhorias.
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk@9001 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrNFSe/ACBrProvedorPublica.pas
+++ Fontes/ACBrNFSe/ACBrProvedorPublica.pas
@@ -106,11 +106,15 @@ begin
   else
     ConfigCidade.NameSpaceEnvelope := 'http://service.nfse.integracao.ws.publica/';
 
-  ConfigCidade.AssinaRPS   := False; // True;
+  case ACodCidade of
+    4208203: ConfigCidade.Identificador := 'id';  // Itajai/SC
+  end;
+
+  ConfigCidade.AssinaRPS   := False;
   ConfigCidade.AssinaLote  := True;
   ConfigCidade.AssinaGerar := True;
 
- Result := ConfigCidade;
+  Result := ConfigCidade;
 end;
 
 function TProvedorPublica.GetConfigSchema(ACodCidade: Integer): TConfigSchema;
